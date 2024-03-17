3bc14c933df62358abec116fb4c0e9802b0d1798
prakash2104 <prakashkumar68553495@gmail.com>
2024-03-17 12:24:02 +0530

    registration page and login page ui built

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,64 @@
-import React from 'react'
-
-function Login() {
+import React from "react";
+import loginpic from "../images/login.png"
+import { NavLink } from "react-router-dom";
+const Login = () => {
   return (
-    <div>
-        <h1>Login</h1>
-    </div>
-  )
-}
+    <>
+      <section className="sign-in">
+        <div className="container mt-5 d-flex justify-content-center">
+          <div className="signin-content">
+          <div className="signin-image">
+              <figure>
+                <img src={loginpic} alt="Login pic" />
+              </figure>
+            </div>  
+            <div className="signin-form">
+              <h2 className="form-title">Sign In</h2>
+              <form className="register-form" id="register-form">
+                <div className="form-group">
+                  <label htmlFor="email">
+                    <i className="zmdi zmdi-account material-icons-name"></i>
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    autoComplete="off"
+                    placeholder="Your email"
+                  />
+                </div>{" "}
+                <div className="form-group">
+                  <label htmlFor="password">
+                    <i className="zmdi zmdi-lock material-icons-name"></i>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    placeholder="Your password"
+                  />
+                </div>
+                <br/>
+                <div className="form-group form-button">
+                  <input
+                    type="submit"
+                    name="signin"
+                    id="signin"
+                    className="form-submit"
+                    value="Sign In"
+                  /><br/>
+                  <NavLink to="/signup" className="signup-image-link">
+                Create an account
+              </NavLink>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,11 +1,123 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import signpic from "../images/registration.png";
 
 function Signup() {
   return (
-    <div>
-        <h1>Signup</h1>
-    </div>
-  )
+    <>
+      <section className="signup">
+        <div className="container mt-5">
+          <div className="signup-content d-flex justify-content-between">
+            <div className="signup-form">
+              <h2 className="form-title">Sign Up</h2>
+              <form className="register-form" id="register-form" >
+                <div className="form-group">
+                  <label htmlFor="name">
+                    <i className="zmdi zmdi-account material-icons-name"></i>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    autoComplete="off"
+                    placeholder="Your name"
+                    style={{ width: "60%", padding: "10px" }}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">
+                    <i className="zmdi zmdi-account material-icons-name"></i>
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    autoComplete="off"
+                    placeholder="Your email"
+                    style={{ width: "60%", padding: "10px" }}
+                  />
+                </div>{" "}
+                <div className="form-group">
+                  <label htmlFor="phone">
+                    <i className="zmdi zmdi-phone-in-talk material-icons-name"></i>
+                  </label>
+                  <input
+                    type="number"
+                    name="phone"
+                    id="phone"
+                    autoComplete="off"
+                    placeholder="Your phone number"
+                    style={{ width: "60%", padding: "10px" }}
+                  />
+                </div>{" "}
+                <div className="form-group">
+                  <label htmlFor="work">
+                    <i className="zmdi zmdi-slideshow material-icons-name"></i>
+                  </label>
+                  <input
+                    type="text"
+                    name="work"
+                    id="work"
+                    autoComplete="off"
+                    placeholder="Your profession"
+                    style={{ width: "60%", padding: "10px" }}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">
+                    <i className="zmdi zmdi-lock material-icons-name"></i>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    placeholder="Your password"
+                    style={{ width: "60%", padding: "10px" }}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="cpassword">
+                    <i className="zmdi zmdi-slideshow material-icons-name"></i>
+                  </label>
+                  <input
+                    type="password"
+                    name="cpassword"
+                    id="cpassword"
+                    autoComplete="off"
+                    placeholder="Confirm Your Password"
+                    style={{ width: "60%", padding: "10px" }}
+                  />
+                </div>
+                <div className="form-group form-button">
+                  <input
+                    type="submit"
+                    name="signup"
+                    id="signup"
+                    className="form-submit"
+                    value="Register"
+                    style={{ width: "60%", padding: "10px" }}
+                  />
+                </div>
+              </form>
+              <NavLink
+                to="/login"
+                className="signup-image-link"
+                style={{ textAlign: "left", display: "block" }}
+              >
+                I am already registered
+              </NavLink>
+            </div>
+            <div className="signup-image">
+              <figure>
+                <img src={signpic} alt="registration" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Signup
+export default Signup;
